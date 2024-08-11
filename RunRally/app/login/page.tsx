989d5e9d5f52e9774b4254bfa19b2fc34a7c0956
Rethakgetse-Manaka <rethakgetse11@gmail.com>
2024-08-11 17:31:49 +0200

    Removed full stop

--- RunRally/app/login/page.tsx
+++ RunRally/app/login/page.tsx
@@ -98,7 +98,6 @@ export default function Login() {
                   >
                     Sign Up
                   </a>
-                  .
                 </p>
 
                 <div className="py-3 flex items-center text-xs text-white uppercase before:flex-1 before:border-t before:border-gray-200 before:me-6 after:flex-1 after:border-t after:border-gray-200 after:ms-6">
