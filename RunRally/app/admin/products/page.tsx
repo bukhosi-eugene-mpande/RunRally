5273d27b59a61eebb50605b5ef52f522c60d5bf0
bukhosi-eugene-mpande <u21573558@tuks.co.za>
2024-09-15 19:36:37 +0200

    updated margins

--- RunRally/app/admin/products/page.tsx
+++ RunRally/app/admin/products/page.tsx
@@ -33,7 +33,7 @@ export default function Dashboard() {
   const products = data.products;
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
-      <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+      <div className="flex flex-col sm:gap-4 sm:py-4">
         <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
           <Tabs defaultValue="all">
             <div className="flex items-center">
